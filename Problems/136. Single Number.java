3f26f913f315900a93034d1562ae2cc8c66599c7
rameshraj55 <67755006+Rameshraj55@users.noreply.github.com>
2023-01-02 12:22:37 +0530

    Update 136. Single Number.java

--- Problems/136. Single Number.java
+++ Problems/136. Single Number.java
@@ -1,5 +1,6 @@
 //Solution #TC #Bit Manipulation 
 /*
+ XOR operator is Commutative.
  1.  A^A=0          A^B^A=B         (A^A^B) = (B^A^A) = (A^B^A) = B This shows that position doesn't matter.
  2.  Similarly , if we see , a^a^a......... (even times)=0 and a^a^a........(odd times)=a
 Why does this work ??
